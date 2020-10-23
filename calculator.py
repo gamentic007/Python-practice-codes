2f1b1c256c597ccb5162c7ebfd71f78447f8bb2c
gamentic007 <73329315+gamentic007@users.noreply.github.com>
2020-10-23 15:45:34 +0530

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,43 @@
+choice = input('''
+Please select the type of operation you want to perform:
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+''')
+ 
+num_1 = int(input('Enter your first number: '))
+num_2 = int(input('Enter your second number: '))
+ 
+if choice == '+':
+    print('{} + {} = '.format(num_1, num_2))
+    print(num_1 + num_2)
+ 
+elif choice == '-':
+    print('{} - {} = '.format(num_1, num_2))
+    print(num_1 - num_2)
+ 
+elif choice == '*':
+    print('{} * {} = '.format(num_1, num_2))
+    print(num_1 * num_2)
+ 
+elif choice == '/':
+    print('{} / {} = '.format(num_1, num_2))
+    print(num_1 / num_2)
+ 
+else:
+    print('Enter a valid operator, please run the program again.')
+
+Output:
+Please select the type of operation you want to perform:
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+ 
+*
+ 
+Please enter the first number: 10
+Please enter the second number: 40
+10 * 40 =
+400
